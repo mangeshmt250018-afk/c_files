02c866580fe18ea401b1ba73f93a2eabf13be972
mangeshmt250018-afk <mangeshm_t250018@students.isquareit.edu.in>
2025-12-10 05:19:40 +0000

    added

--- c_files/momentum.c
+++ c_files/momentum.c
@@ -3,15 +3,14 @@
 
 int main()
 {
-float mass , energy , velocity_of_light ,momentum,velocity  ;
-printf("Enter the mass :");
-scanf("%f",&mass);
-printf("Enter the velocity :");
-scanf("%f",&velocity);
-velocity_of_light = 3*pow(10,8) ;
-momentum = mass * velocity  ;
-energy = mass *   velocity_of_light * velocity_of_light  ;
-printf("The energy of the mass %f is %0.2f \n",mass,energy);
-printf("The momentum of the mass %f is %0.2f",mass,momentum);
+float mass, energy, velocity_of_light, momentum, velocity;
+printf("Enter the mass: ");
+scanf("%f", &mass);
+printf("Enter the velocity: ");
+scanf("%f", &velocity);
+velocity_of_light = 3 * pow(10, 8);
+momentum = mass * velocity;
+energy = mass * velocity_of_light * velocity_of_light;
+printf("The energy of the mass %f is %e Joules\n", mass, energy);
+printf("The momentum of the mass %f is %0.2f kg·m/s", mass, momentum);
 }
-
